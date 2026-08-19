b655a4c0d71bdcc232d711f591735306d5e46e17
antonio_mont <antoniomontrz13@gmail.com>
2026-08-18 23:35:08 -0600

    feat: add focus timer

--- frontend/src/storage/focusStorage.js
+++ frontend/src/storage/focusStorage.js
@@ -1,34 +1,15 @@
 import {
   createUserStorageKey,
-  isPlainObject,
   readStoredJson,
   removeStoredValue,
   writeStoredJson,
 } from "./storageUtils.js";
+import {
+  isValidFocusState,
+  normalizeFocusState,
+} from "../utils/focusUtils.js";
 
 const FOCUS_STORAGE_NAMESPACE = "blue:tools:focus:v1";
-
-function createInitialFocusSettings() {
-  return {
-    workMinutes: 20,
-    restMinutes: 5,
-    soundEnabled: true,
-    completedSessions: 0,
-  };
-}
-
-function isValidFocusSettings(value) {
-  return (
-    isPlainObject(value) &&
-    Number.isInteger(value.workMinutes) &&
-    value.workMinutes > 0 &&
-    Number.isInteger(value.restMinutes) &&
-    value.restMinutes >= 0 &&
-    typeof value.soundEnabled === "boolean" &&
-    Number.isInteger(value.completedSessions) &&
-    value.completedSessions >= 0
-  );
-}
 
 function getStorageKey(userId) {
   return createUserStorageKey(FOCUS_STORAGE_NAMESPACE, userId);
@@ -36,30 +17,15 @@ function getStorageKey(userId) {
 
 export function getFocusSettings(userId) {
   const storedSettings = readStoredJson(getStorageKey(userId));
-
-  if (!isValidFocusSettings(storedSettings)) {
-    return createInitialFocusSettings();
-  }
-
-  return {
-    workMinutes: storedSettings.workMinutes,
-    restMinutes: storedSettings.restMinutes,
-    soundEnabled: storedSettings.soundEnabled,
-    completedSessions: storedSettings.completedSessions,
-  };
+  return normalizeFocusState(storedSettings);
 }
 
 export function saveFocusSettings(userId, data) {
-  if (!isValidFocusSettings(data)) {
+  if (!isValidFocusState(data)) {
     return false;
   }
 
-  return writeStoredJson(getStorageKey(userId), {
-    workMinutes: data.workMinutes,
-    restMinutes: data.restMinutes,
-    soundEnabled: data.soundEnabled,
-    completedSessions: data.completedSessions,
-  });
+  return writeStoredJson(getStorageKey(userId), data);
 }
 
 export function removeFocusSettings(userId) {
